f946a4a22b403a929ca89b9c733ff3d53c29898a
유연석 <ysyoo627@naver.com>
2023-10-25 10:56:12 +0900

    fe/ref: refactor value of url variable

--- front-end/src/apis/urls.ts
+++ front-end/src/apis/urls.ts
@@ -5,11 +5,11 @@ const PORT_SERVER = ':8081';
 const PORT_OPENVIDU = ':4443';
 
 const URL_LOCAL = 'http://localhost';
-const URL_RELEASE = 'https://i9c207.p.ssafy.io';
+const URL_RELEASE = 'https://k9c103.p.ssafy.io';
 
 // export const WS_BASE_URL =
-//   (DEV ? `ws://localhost${PORT_SERVER}` : 'wss://i9c207.p.ssafy.io') + '/ws';
-export const WS_BASE_URL = 'wss://i9c207.p.ssafy.io' + '/ws';
+//   (DEV ? `ws://localhost${PORT_SERVER}` : 'wss://k9c103.p.ssafy.io') + '/ws';
+export const WS_BASE_URL = 'wss://k9c103.p.ssafy.io' + '/ws';
 
 // export const API_BASE_URL = (DEV ? URL_RELEASE : URL_RELEASE) + '/api/v1';
 export const API_BASE_URL = URL_RELEASE + '/api/v1';
