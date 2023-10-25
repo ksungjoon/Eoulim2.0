7a9a4e9930e693d87ed411cdc906dce71941aefd
김성준 <gkxm0443@naver.com>
2023-10-25 10:33:04 +0900

    fe/feat : 개발환경설정

---
+++ mobile/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:mobile/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Login(),
+        }),
+  );
+}
---
+++ mobile/lib/screen/login_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  // TextEditingController를 사용하여 입력 필드의 값을 관리
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('로그인'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 사용자명 입력 필드
+            TextFormField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: '사용자명',
+              ),
+            ),
+            // 비밀번호 입력 필드
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: '비밀번호',
+              ),
+              obscureText: true, // 비밀번호를 숨기기
+            ),
+            SizedBox(height: 20),
+            // 로그인 버튼
+            ElevatedButton(
+              onPressed: () {
+                // 여기에 로그인 로직을 추가하세요
+                // 사용자명과 비밀번호는 _usernameController.text와 _passwordController.text로 얻을 수 있습니다.
+              },
+              child: Text('로그인'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
