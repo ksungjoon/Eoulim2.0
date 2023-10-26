cd89286e4f4cfb667f3accdc78de554f2196c7ec
유연석 <ysyoo627@naver.com>
2023-10-26 15:01:24 +0900

    be/ref: refactoring toString method of errorResponse

--- back-end/src/main/java/com/ssafy/eoullim/dto/response/ErrorResponse.java
+++ back-end/src/main/java/com/ssafy/eoullim/dto/response/ErrorResponse.java
@@ -3,11 +3,9 @@ package com.ssafy.eoullim.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@ToString
 @NoArgsConstructor
 public class ErrorResponse {    // 반환할 Error Response 객체
     private HttpStatus status;
@@ -19,5 +17,14 @@ public class ErrorResponse {    // 반환할 Error Response 객체
         this.status = status;
         this.code = code;
         this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"status\":" + "\"" + status + "\"," +
+                "\"code\":" + "\"" + code + "\"," +
+                "\"message\":" + "\"" + message + "\"," +
+                "}";
     }
 }
