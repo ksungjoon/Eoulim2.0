4840677e992a4c62874007fd82ed0166c175fdc8
유연석 <ysyoo627@naver.com>
2023-10-26 15:00:24 +0900

    be/ref: refactoring global exception handler

--- back-end/src/main/java/com/ssafy/eoullim/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/ssafy/eoullim/exception/GlobalExceptionHandler.java
@@ -24,15 +24,11 @@ public class GlobalExceptionHandler {
         ErrorResponse.builder()
             .status(e.getErrorCode().getStatus())
             .code(e.getErrorCode().getCode())
-            .message(e.getErrorCode().getMessage()) // 클라이언트에게는 에러 코드만.
-            //            .message(
-            //                String.format(
-            //                    "errorCode msg: %s. \n exception msg: %s",
-            //                    e.getErrorCode().getMessage(), e.getMessage()))   // 이건 exception과
-            // error code를 모두 client에게 보여줌
+            .message(e.getMessage()) // 클라이언트에게는 에러 코드만.
             .build();
     // 여기서 ERROR CODE 안에 있는 status랑 message, 또한 추가로 exception으로 온 message
-    log.error("Error occurs {}", e.getMessage()); // 서버에 exception 메시지 출력
+    log.error("Error occurs {}", e.toString()); // 서버에 exception 메시지 출력
+    log.error("Error occurs in method: " + e.getStackTrace()[0]);
     return ResponseEntity.status(e.getErrorCode().getStatus()).body(errorResponse);
   }
 
@@ -48,50 +44,38 @@ public class GlobalExceptionHandler {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
   }
 
-  @ExceptionHandler(IOException.class)
-  public ResponseEntity<ErrorResponse> serverErrorHandler(IOException e) {
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleAll(Exception e) {
     final var errorResponse =
-            ErrorResponse.builder()
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                    .message(e.getMessage())
-                    .build();
+        ErrorResponse.builder()
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+            .message(e.getMessage())
+            .build();
     log.error("Error occurs {}", e.toString());
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
   }
 
-  //  @ExceptionHandler(IllegalArgumentException.class)
-  //  public ResponseEntity<?> databaseErrorHandler(IllegalArgumentException e) {
-  //    log.error("Error occurs {}", e.toString());
-  //    return ResponseEntity.status(DATABASE_ERROR.getStatus())
-  //        .body(Response.error(DATABASE_ERROR.name()));
-  //  }
+  @ExceptionHandler(IOException.class)
+  public ResponseEntity<ErrorResponse> serverErrorHandler(IOException e) {
+    final var errorResponse =
+        ErrorResponse.builder()
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+            .message(e.getMessage())
+            .build();
+    log.error("Error occurs {}", e.toString());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
 
-  @ExceptionHandler(OpenViduHttpException.class)
+  @ExceptionHandler({
+    OpenViduHttpException.class,
+    OpenViduJavaClientException.class,
+    InterruptedException.class
+  })
   public ResponseEntity<?> openviduErrorHandler(OpenViduHttpException e) {
     log.error("Error occurs {}", e.toString());
     return ResponseEntity.status(OPENVIDU_HTTP_ERROR.getStatus())
         .body(Response.error(OPENVIDU_HTTP_ERROR.name()));
-  }
-
-  @ExceptionHandler(OpenViduJavaClientException.class)
-  public ResponseEntity<?> openviduErrorHandler(OpenViduJavaClientException e) {
-    log.error("Error occurs {}", e.toString());
-    return ResponseEntity.status(OPENVIDU_SERVER_ERROR.getStatus())
-        .body(Response.error(OPENVIDU_SERVER_ERROR.name()));
-  }
-
-  @ExceptionHandler(InterruptedException.class)
-  public ResponseEntity<?> InterruptHandler(InterruptedException e) {
-    log.error("Error occurs {}", e.toString());
-    return ResponseEntity.status(OPENVIDU_SERVER_ERROR.getStatus())
-        .body(Response.error(OPENVIDU_SERVER_ERROR.name()));
-  }
-
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<?> exceptionHandler(Exception e) {
-    log.error("Error occurs {}", e.toString());
-    return ResponseEntity.status(OPENVIDU_SERVER_ERROR.getStatus())
-        .body(Response.error(OPENVIDU_SERVER_ERROR.name()));
   }
 }
