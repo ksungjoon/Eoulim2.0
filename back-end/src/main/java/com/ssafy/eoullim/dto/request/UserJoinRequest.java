11fb0a5cd448282e9e7dca5186eaf6782b32c50c
유연석 <ysyoo627@naver.com>
2023-10-27 14:30:45 +0900

    be/ref: refactoring user join request validation

--- back-end/src/main/java/com/ssafy/eoullim/dto/request/UserJoinRequest.java
+++ back-end/src/main/java/com/ssafy/eoullim/dto/request/UserJoinRequest.java
@@ -1,6 +1,7 @@
 package com.ssafy.eoullim.dto.request;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,13 +11,19 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 public class UserJoinRequest {
   @NotBlank
-  @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9]{2,19}") // 첫 글자는 영어 대소문자, 그 뒤는 영어 대소문자 혹은 숫자 3~20자
+  @Pattern(
+      regexp = "[a-zA-Z0-9]{3,20}",
+      message = "아이디는 영어 대소문자 혹은 숫자만 입력 가능합니다. (3~20자)") // 첫 글자는 영어 대소문자, 그 뒤는 영어 대소문자 혹은 숫자 3~20자
   String username;
 
   @NotBlank
   //    @Pattern(regexp = "[a-zA-Z0-9]{8,20}")
   @Pattern(
-      regexp = "^(?=(.*[0-9]){1,})(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[@#$%^&+=]){0,}).{8,20}$")
+      regexp = "^(?=(.*[0-9]){1,})(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[@#$%^&+=]){0,}).{8,20}$",
+      message =
+          "문자열은 숫자([0-9]), 소문자([a-z]), 대문자([A-Z]), 특수 문자([@#$%^&+=]) 중 3가지 이상을 포함해야 합니다.\n"
+              + "문자열은 총 길이가 8자 이상 20자 이하 이어야 합니다.")
+  @Length(min = 3, max = 20)
   String password;
 
   @NotBlank String name;
