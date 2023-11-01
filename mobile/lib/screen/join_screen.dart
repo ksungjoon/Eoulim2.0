0e9a7fee7652bb24c2a4f92686e86ffd12fac121
blue4683 <an4683@gmail.com>
2023-11-01 13:35:35 +0900

    fe/feat: add webview widget
    this commit adds webview widget.

--- mobile/lib/screen/join_screen.dart
+++ mobile/lib/screen/join_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile/screen/session_screen.dart';
 
 class Join extends StatefulWidget {
-  const Join({Key? key});
+  const Join({super.key});
 
   @override
   State<Join> createState() => _JoinState();
@@ -13,10 +13,10 @@ class _JoinState extends State<Join> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('새 친구'),
+        title: const Text('새 친구'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -24,13 +24,11 @@ class _JoinState extends State<Join> {
             ElevatedButton(
               onPressed: () {
                 // 여기에 로그인 로직을 추가하세요
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const Session(title: "새 친구")));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Session()));
                 // 사용자명과 비밀번호는 _usernameController.text와 _passwordController.text로 얻을 수 있습니다.
               },
-              child: Text('새 친구'),
+              child: const Text('새 친구'),
             ),
           ],
         ),
