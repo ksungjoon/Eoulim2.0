1f8b7dd5624d392b21bd06e10e8768cc0701874b
blue4683 <an4683@gmail.com>
2023-11-02 11:43:01 +0900

    fe/feat: add alarm function by using fcm
    this commit adds alarm function by using firebase cloud messaging.

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,14 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'firebase_options.dart';
+
 import 'package:mobile/screen/splash_screen.dart';
 
-void main() {
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // await Firebase.initializeApp();
+  // await setupFlutterNotifications();
+  // showFlutterNotification(message);
+}
+
+@pragma('vm:entry-point')
+void backgroundHandler(NotificationResponse details) {
+  // 액션 추가... 파라미터는 details.payload 방식으로 전달
+}
+
+void initializeNotification() async {
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(const AndroidNotificationChannel(
+          'high_importance_channel', 'high_importance_notification',
+          importance: Importance.max));
+
+  await flutterLocalNotificationsPlugin.initialize(
+    const InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+      iOS: DarwinInitializationSettings(),
+    ),
+    onDidReceiveBackgroundNotificationResponse: backgroundHandler,
+  );
+
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+    RemoteNotification? notification = message.notification;
+
+    if (notification != null) {
+      Future.delayed(const Duration(milliseconds: 3000), () {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            const NotificationDetails(
+              android: AndroidNotificationDetails(
+                'high_importance_channel',
+                'high_importance_notification',
+                importance: Importance.max,
+              ),
+              iOS: DarwinNotificationDetails(),
+            ),
+            payload: message.data['test_paremeter1']);
+        print(notification.body);
+        print("수신자 측 메시지 수신");
+      });
+    }
+  });
+
+  RemoteMessage? message = await FirebaseMessaging.instance.getInitialMessage();
+  if (message != null) {
+    // 액션 부분 -> 파라미터는 message.data['test_parameter1'] 이런 방식으로...
+  }
+}
+
+void getMyDeviceToken() async {
+  final token = await FirebaseMessaging.instance.getToken();
+  print("내 디바이스 토큰: $token");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  initializeNotification();
+
+  getMyDeviceToken();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -27,16 +108,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// void main() async {
-//   runApp(
-//     MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Eoullim',
-//       initialRoute: '/',
-//       routes: {
-//         '/': (context) => Login(),
-//       },
-//     ),
-//   );
-// }
--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import flutter_secure_storage_macos
 import flutter_webrtc
 import openvidu_client
@@ -13,6 +16,9 @@ import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   OpenviduClientPlugin.register(with: registry.registrar(forPlugin: "OpenviduClientPlugin"))
