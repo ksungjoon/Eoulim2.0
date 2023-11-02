ec212316560cf31dd36c785d32696f6fa0a569fc
김하영 <bnk159hair@naver.com>
2023-11-02 10:04:09 +0900

    be/refactor : add swagger docs

--- back-end/src/main/java/com/ssafy/eoullim/configuration/SwaggerConfig.java
+++ back-end/src/main/java/com/ssafy/eoullim/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.eoullim.utils;
+package com.ssafy.eoullim.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
