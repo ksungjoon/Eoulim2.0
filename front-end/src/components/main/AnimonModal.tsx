5cffb303b008b6fe8f794135d7084f320b0dc076
유연석 <ysyoo627@naver.com>
2023-11-03 15:32:21 +0900

    fe/ref: child - animon list

--- front-end/src/components/main/AnimonModal.tsx
+++ front-end/src/components/main/AnimonModal.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import {
-  ModalOverlay,
-  ModalContent,
-  Cat,
-  Dog,
-  Panda,
-  Tiger,
-} from './AnimonModalStyles';
+import React, { useEffect, useState } from 'react';
+import { ModalOverlay, ModalContent, Cat, Dog, Panda, Tiger } from './AnimonModalStyles';
 import { useRecoilValue } from 'recoil';
 import { Profilekey } from '../../atoms/Profile';
 import { tokenState } from '../../atoms/Auth';
@@ -16,11 +9,18 @@ import { API_BASE_URL } from '../../apis/urls';
 interface AnimonModalProps {
   onClose: () => void;
   profile: () => void;
+  animons: {
+    id: number;
+    headImagePath: string;
+    bodyImagePath: string;
+    name: string;
+  };
 }
 
-const AnimonModal: React.FC<AnimonModalProps> = ({ onClose, profile }) => {
+const AnimonModal = ({ onClose, profile }: AnimonModalProps) => {
   const profileId = useRecoilValue(Profilekey);
   const token = useRecoilValue(tokenState);
+  const [animons, setAnimons] = useState([]);
 
   const changeAnimon = (id: number) => {
     axios
@@ -34,14 +34,34 @@ const AnimonModal: React.FC<AnimonModalProps> = ({ onClose, profile }) => {
         profile();
         console.log('바꾸기 완료');
       })
-      .catch((error) => {
+      .catch(error => {
         console.log('바꾸기 요청 오류', error);
       });
   };
 
+  const getAnimon = () => {
+    axios
+      .get(`${API_BASE_URL}/children/${profileId}/animons`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(response => {
+        setAnimons(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getAnimon();
+  }, [animons]);
+
   return (
     <>
-      <ModalOverlay onClick={onClose} >
+      <ModalOverlay onClick={onClose}>
         <ModalContent>
           <Panda onClick={() => changeAnimon(1)}></Panda>
           <Dog onClick={() => changeAnimon(2)}></Dog>
