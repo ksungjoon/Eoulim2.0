8af0fd02e801c927c04bfc71d02c63b2763c2d1d
오영재 <dhdnqkrtk22@naver.com>
2023-11-03 10:22:58 +0900

    fe/style: add background image and change button color

--- mobile/lib/screen/enter_screen.dart
+++ mobile/lib/screen/enter_screen.dart
@@ -12,40 +12,62 @@ class _EnterState extends State<Enter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/bear.png'),
-            const SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Session(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/enter.gif'),
+              fit: BoxFit.cover,
+              opacity: 0.5),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/bear.png'),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color(0xff16A085),
+                      Color(0xffF4D03F),
+                    ],
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xff123456),
-                minimumSize: const Size(400, 150),
-                maximumSize: const Size(600, 300),
-                shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16.0),
                 ),
-              ),
-              child: const Text(
-                '새 친구 만나기',
-                style: TextStyle(
-                  fontSize: 36,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Session(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    minimumSize: const Size(400, 150),
+                    maximumSize: const Size(600, 300),
+                  ),
+                  child: const Text(
+                    '새 친구 만나기',
+                    style: TextStyle(
+                      fontSize: 36,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
