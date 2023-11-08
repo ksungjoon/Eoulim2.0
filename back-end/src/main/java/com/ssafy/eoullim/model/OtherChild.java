fb895b1a33146c8745c1ee551e40a40bbeb9e636
유연석 <ysyoo627@naver.com>
2023-11-08 09:54:31 +0900

    be/ref: refactoring other-child class with builder

--- back-end/src/main/java/com/ssafy/eoullim/model/OtherChild.java
+++ back-end/src/main/java/com/ssafy/eoullim/model/OtherChild.java
@@ -1,9 +1,13 @@
 package com.ssafy.eoullim.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class OtherChild {
     private Long id;
@@ -14,14 +18,14 @@ public class OtherChild {
     private Animon profileAnimon;
 
     public static OtherChild fromChild(Child child) {
-        return new OtherChild(
-                child.getId(),
-                child.getName(),
-                child.getGender(),
-                child.getSchool(),
-                child.getGrade(),
-                child.getProfileAnimon()
-        );
+        return OtherChild.builder()
+                .id(child.getId())
+                .name(child.getName())
+                .gender(child.getGender())
+                .school(child.getSchool())
+                .grade(child.getGrade())
+                .profileAnimon(child.getProfileAnimon())
+                .build();
     }
 }
 
