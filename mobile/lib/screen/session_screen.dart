96c1642e929f7923eed8ddcbb6923e7778dcd0d8
blue4683 <an4683@gmail.com>
2023-11-10 13:44:24 +0900

    mobile/feat: connect web session in mobile

--- mobile/lib/screen/session_screen.dart
+++ mobile/lib/screen/session_screen.dart
@@ -4,6 +4,9 @@
 
 // ignore_for_file: public_member_api_docs
 
+import 'dart:ffi';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -18,52 +21,67 @@ class Session extends StatefulWidget {
 }
 
 class _SessionState extends State<Session> {
-  late final WebViewController controller;
+  late final WebViewController _controller;
+  late String childId = '';
   late String token = '';
+  final bool invitation = false;
 
-  Future<void> getToken() async {
-    final storage = new FlutterSecureStorage();
+  Future<void> getChildInfo() async {
+    const storage = FlutterSecureStorage();
     String? authKey = await storage.read(key: 'Authkey');
+    String? profileId = await storage.read(key: 'childId');
     setState(() {
       if (authKey != null) {
         token = authKey;
       } else {
+        print('토큰이 없습니다.');
         token = '';
+      }
+
+      if (profileId != null) {
+        childId = profileId;
+      } else {
+        print('아이디가 없습니다.');
+        childId = '';
       }
     });
   }
 
-  void sendToken() {
-    if (token != '') {
-      print('웹으로 보내는 중....');
-      Map<String, String> message = {'token': token};
-      controller.runJavaScript('getTokenFromApp("$message")');
+  void sendChildId() async {
+    if (childId != '') {
+      print('웹으로 childId 보내는 중입니다.');
+      String message = json
+          .encode({"childId": childId, "invitation": false, "token": token});
+      await _controller.runJavaScript("changePage('$message')");
     }
   }
 
   @override
   void initState() {
     super.initState();
-    getToken();
+    getChildInfo();
 
     // #docregion webview_controller
-    controller = WebViewController()
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
         NavigationDelegate(
           onProgress: (int progress) {
-            print('token: $token');
-            sendToken();
+            _controller.currentUrl().then((result) => {print(result)});
+            print('profileId: $childId');
+            sendChildId();
           },
           onPageStarted: (String url) {},
           onPageFinished: (String url) {
             debugPrint('Page Finished');
           },
-          onWebResourceError: (WebResourceError error) {},
+          onWebResourceError: (WebResourceError error) {
+            print(error);
+          },
         ),
       )
-      ..loadRequest(Uri.parse('https://k9c103.p.ssafy.io/session'));
+      ..loadRequest(Uri.parse('https://k9c103.p.ssafy.io/mobile'));
     // #enddocregion webview_controller
   }
 
@@ -72,7 +90,7 @@ class _SessionState extends State<Session> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter Simple Example')),
-      body: WebViewWidget(controller: controller),
+      body: WebViewWidget(controller: _controller),
     );
   }
   // #enddocregion webview_widget
