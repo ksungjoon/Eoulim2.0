ffdcadc9c996389ca0814a524e2b3df536507e87
오영재 <dhdnqkrtk22@naver.com>
2023-11-14 00:07:53 +0900

    fe/chore: add fcm dialog at home screen and delete profile dialog

--- mobile/lib/screen/settings_screen.dart
+++ mobile/lib/screen/settings_screen.dart
@@ -25,50 +25,94 @@ class SettingsScreen extends StatelessWidget {
           child: SafeArea(
             child: Padding(
               padding:
-                  const EdgeInsets.symmetric(vertical: 40.0, horizontal: 30.0),
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _buildButtonRow(
-                    '프로필 정보 수정',
-                    onPressed: () {
-                      Get.to(() => const ModifyProfileScreen());
-                    },
+                  Column(
+                    children: [
+                      _buildButtonRow(
+                        '프로필 정보 수정',
+                        onPressed: () {
+                          Get.to(() => const ModifyProfileScreen());
+                        },
+                      ),
+                      _buildButtonRow(
+                        '프로필 전환',
+                        onPressed: profileLogout,
+                      ),
+                    ],
                   ),
-                  _buildButtonRow(
-                    '프로필 정보 삭제',
-                    onPressed: () async {
-                      final response = await ApiProfile.deleteProfile();
-                      if (response == 204) {
-                        profileLogout();
-                      } else if (response == 401) {
-                        userLogout();
-                      } else {
-                        showDialog(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext ctx) {
-                            return AlertDialog(
-                              content: const Text('알 수 없는 오류'),
-                              actions: [
-                                Center(
-                                  child: TextButton(
-                                    child: const Text('확인'),
-                                    onPressed: () {
-                                      profileLogout();
-                                      userLogout();
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: TextButton(
+                          onPressed: () {
+                            Get.defaultDialog(
+                              title: '정말로 삭제하시겠습니까?',
+                              middleText: '되돌릴 수 없습니다!',
+                              textConfirm: '삭제',
+                              textCancel: '취소',
+                              buttonColor: Colors.red,
+                              confirmTextColor: Colors.white,
+                              cancelTextColor: Colors.red,
+                              onConfirm: () async {
+                                final response =
+                                    await ApiProfile.deleteProfile();
+                                if (response == 204) {
+                                  profileLogout();
+                                } else if (response == 401) {
+                                  userLogout();
+                                } else {
+                                  showDialog(
+                                    context: context,
+                                    barrierDismissible: false,
+                                    builder: (BuildContext ctx) {
+                                      return AlertDialog(
+                                        content: const Text('알 수 없는 오류'),
+                                        actions: [
+                                          Center(
+                                            child: TextButton(
+                                              child: const Text('확인'),
+                                              onPressed: () {
+                                                profileLogout();
+                                                userLogout();
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      );
                                     },
-                                  ),
-                                ),
-                              ],
+                                  );
+                                }
+                              },
                             );
                           },
-                        );
-                      }
-                    },
-                  ),
-                  _buildButtonRow(
-                    '프로필 전환',
-                    onPressed: profileLogout,
+                          style: TextButton.styleFrom(
+                            side: BorderSide(
+                              color: Colors.black.withOpacity(0.9),
+                              width: 1.0,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 14,
+                              horizontal: 80,
+                            ),
+                          ),
+                          child: Text(
+                            '프로필 정보 삭제',
+                            style: TextStyle(
+                              color: Colors.black.withOpacity(0.9),
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -87,7 +131,8 @@ Widget _buildButtonRow(String buttonText, {VoidCallback? onPressed}) {
     highlightColor: Colors.transparent,
     child: Padding(
       padding: const EdgeInsets.only(
-        bottom: 50.0,
+        bottom: 25.0,
+        top: 25.0,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
