c4132cf39f7eb55dfc15a82a7b390ba4646d23ac
고우민 <kwm907@gmail.com>
2023-11-13 10:24:27 +0900

    fix: NotificationController Responsebody

--- back-end/src/main/java/com/ssafy/eoullim/controller/NotificationController.java
+++ back-end/src/main/java/com/ssafy/eoullim/controller/NotificationController.java
@@ -10,9 +10,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@ResponseBody
 @RequestMapping("/api/v1/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
--- back-end/src/main/java/com/ssafy/eoullim/repository/UserCacheRepository.java
+++ back-end/src/main/java/com/ssafy/eoullim/repository/UserCacheRepository.java
@@ -16,7 +16,7 @@ public class UserCacheRepository {
 
     private final RedisTemplate<String, User> userRedisTemplate;
 
-    private final static Duration USER_CACHE_TTL = Duration.ofHours(10);
+    private final static Duration USER_CACHE_TTL = Duration.ofDays(30);
 
     public void setUser(User user) {
         String key = getKey(user.getUsername());
