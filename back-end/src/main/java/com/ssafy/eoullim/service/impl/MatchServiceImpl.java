073bcd2b577d0d2fa3355a0553dab982fb2560cb
김하영 <bnk159hair@naver.com>
2023-11-13 09:13:59 +0900

    be/feat fix match

--- back-end/src/main/java/com/ssafy/eoullim/service/impl/MatchServiceImpl.java
+++ back-end/src/main/java/com/ssafy/eoullim/service/impl/MatchServiceImpl.java
@@ -72,101 +72,101 @@ public class MatchServiceImpl implements MatchService {
     this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
   }
 
-  @Override
-  public void startRandom_01(MatchStartRequest matchStartRequest){
-    // Child ID가 현재 User의 Child가 맞는지 체크
-    matchList.add(MatchWait.builder().childId(matchStartRequest.getChildId()).grade(matchStartRequest.getGrade()).priority((short)0).build());
-  }
-
-  @Scheduled(fixedDelay = 10000)
-  public void makeMatch(){
-    int initLen = matchList.size(); // 현재 큐에 있는 값만 빼기 위해서 - 함수 실행 중 큐에 들어오는 애들은 다음 스케쥴 때 진행할거여서 상관없음
-    List<MatchWait> curList = new ArrayList<>(); // 큐에서 뺀 애들 저장하기 위해
-    List<MatchWait> leftList = new ArrayList<>();
-
-    boolean [] visited = new boolean[initLen];
-    Arrays.fill(visited, false);
-    int []cntNum = new int[4];
-    int startIdx = 0;
-
-    for(int i = 0; i < initLen; i++){
-      curList.add(matchList.poll()); // 큐에서는 현재 개수만큼 빼고 새로 옮기기
-    }
-    Collections.sort(curList); // 학년 -> 우선순위, 둘다 내림차순 정렬
-    for (MatchWait match : curList){
-      cntNum[(int)match.getGrade()]++; // 학년 개수 count
-    }
-
-    for(int i=1; i< 4; i++){
-      if (cntNum[i] < 2){ // 학년 인원이 2인 아래면 넘기기
-        startIdx += cntNum[i];
-        continue;
-      }
-      for(int j=0; j < cntNum[i]; j++){ // 짝수 인원만큼 매칭
-        if(j == cntNum[j] - 1){
-          MatchWait left = curList.get(startIdx+j);
-          short newPriority = (short)(left.getGrade()+1);
-          left.setPriority(newPriority);
-          leftList.add(left);
-          continue;
-        }
-        Long childOne = curList.get(startIdx+j).getChildId();
-        Long childTwo = curList.get(startIdx+j+1).getChildId();
-        j += 1;
-        sendMatchResult(childOne, childTwo);
-      }
-
-      startIdx += cntNum[i];
-    }
-    //------------------------------------------------------------//
-//    for(int i =0; i< visited.length ; i++){
-//      if(visited[i])continue;
-//      MatchWait left = curList.get(i);
-//      short newPriority = (short)(left.getGrade()+1);
-//      left.setPriority(newPriority);
-//      leftList.add(left);
+//  @Override
+//  public void startRandom_01(MatchStartRequest matchStartRequest){
+//    // Child ID가 현재 User의 Child가 맞는지 체크
+//    matchList.add(MatchWait.builder().childId(matchStartRequest.getChildId()).grade(matchStartRequest.getGrade()).priority((short)0).build());
+//  }
+//
+//  @Scheduled(fixedDelay = 10000)
+//  public void makeMatch(){
+//    int initLen = matchList.size(); // 현재 큐에 있는 값만 빼기 위해서 - 함수 실행 중 큐에 들어오는 애들은 다음 스케쥴 때 진행할거여서 상관없음
+//    List<MatchWait> curList = new ArrayList<>(); // 큐에서 뺀 애들 저장하기 위해
+//    List<MatchWait> leftList = new ArrayList<>();
+//
+//    boolean [] visited = new boolean[initLen];
+//    Arrays.fill(visited, false);
+//    int []cntNum = new int[4];
+//    int startIdx = 0;
+//
+//    for(int i = 0; i < initLen; i++){
+//      curList.add(matchList.poll()); // 큐에서는 현재 개수만큼 빼고 새로 옮기기
 //    }
-    leftList.sort(new Comparator<MatchWait>() { // 우선순위로 정렬
-      @Override
-      public int compare(MatchWait o1, MatchWait o2) {
-        return o2.getPriority()- o1.getPriority();
-      }
-    });
-    
-    for(int i = 0; i < leftList.size(); i++){
-      if(i == leftList.size()-1){
-        matchList.add(leftList.get(i));
-        continue;
-      }
-      MatchWait left = leftList.get(i);
-      if(left.getPriority()>5){
-        sendMatchResult(left.getChildId(), leftList.get(i+1).getChildId());
-        i += 1; // 두 개 읽었으므로 인덱스 하나 건너뜀
-      }
-      else{
-        matchList.add(left);
-      }
-    }
-  }
-
-
-  public void sendMatchResult(Long childOne, Long childTwo){
-    // session ID 짓기
-    String sessionId = buildSessionId(childOne);
-    // Session Id를 통해 Open Vidu Session 생성
-    Session session = createNewOpenViduSession(sessionId);
-    mapSessions.put(sessionId, session);
-    // Connection Token 생성
-    String tokenOne = creatConnectionToken(session);
-    String tokenTwo = creatConnectionToken(session);
-
-    Room newRoom = newMakeNewRoom(sessionId, true, childOne, childTwo);
-    mapRooms.put(sessionId, newRoom);
-
-    simpMessageSendingOperations.convertAndSend("/topic/random/start"+childOne, new Match(sessionId, tokenOne, newRoom.getRandom()));
-    simpMessageSendingOperations.convertAndSend("/topic/random/start"+childTwo, new Match(sessionId, tokenTwo, newRoom.getRandom()));
-
-  }
+//    Collections.sort(curList); // 학년 -> 우선순위, 둘다 내림차순 정렬
+//    for (MatchWait match : curList){
+//      cntNum[(int)match.getGrade()]++; // 학년 개수 count
+//    }
+//
+//    for(int i=1; i< 4; i++){
+//      if (cntNum[i] < 2){ // 학년 인원이 2인 아래면 넘기기
+//        startIdx += cntNum[i];
+//        continue;
+//      }
+//      for(int j=0; j < cntNum[i]; j++){ // 짝수 인원만큼 매칭
+//        if(j == cntNum[j] - 1){
+//          MatchWait left = curList.get(startIdx+j);
+//          short newPriority = (short)(left.getGrade()+1);
+//          left.setPriority(newPriority);
+//          leftList.add(left);
+//          continue;
+//        }
+//        Long childOne = curList.get(startIdx+j).getChildId();
+//        Long childTwo = curList.get(startIdx+j+1).getChildId();
+//        j += 1;
+//        sendMatchResult(childOne, childTwo);
+//      }
+//
+//      startIdx += cntNum[i];
+//    }
+//    //------------------------------------------------------------//
+////    for(int i =0; i< visited.length ; i++){
+////      if(visited[i])continue;
+////      MatchWait left = curList.get(i);
+////      short newPriority = (short)(left.getGrade()+1);
+////      left.setPriority(newPriority);
+////      leftList.add(left);
+////    }
+//    leftList.sort(new Comparator<MatchWait>() { // 우선순위로 정렬
+//      @Override
+//      public int compare(MatchWait o1, MatchWait o2) {
+//        return o2.getPriority()- o1.getPriority();
+//      }
+//    });
+//
+//    for(int i = 0; i < leftList.size(); i++){
+//      if(i == leftList.size()-1){
+//        matchList.add(leftList.get(i));
+//        continue;
+//      }
+//      MatchWait left = leftList.get(i);
+//      if(left.getPriority()>5){
+//        sendMatchResult(left.getChildId(), leftList.get(i+1).getChildId());
+//        i += 1; // 두 개 읽었으므로 인덱스 하나 건너뜀
+//      }
+//      else{
+//        matchList.add(left);
+//      }
+//    }
+//  }
+//
+//
+//  public void sendMatchResult(Long childOne, Long childTwo){
+//    // session ID 짓기
+//    String sessionId = buildSessionId(childOne);
+//    // Session Id를 통해 Open Vidu Session 생성
+//    Session session = createNewOpenViduSession(sessionId);
+//    mapSessions.put(sessionId, session);
+//    // Connection Token 생성
+//    String tokenOne = creatConnectionToken(session);
+//    String tokenTwo = creatConnectionToken(session);
+//
+//    Room newRoom = newMakeNewRoom(sessionId, true, childOne, childTwo);
+//    mapRooms.put(sessionId, newRoom);
+//
+//    simpMessageSendingOperations.convertAndSend("/topic/random/start"+childOne, new Match(sessionId, tokenOne, newRoom.getRandom()));
+//    simpMessageSendingOperations.convertAndSend("/topic/random/start"+childTwo, new Match(sessionId, tokenTwo, newRoom.getRandom()));
+//
+//  }
 
 
 
